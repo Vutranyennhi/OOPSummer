a174d99841007460b86e6b381b774738e7692faf
Vutranyennhi <135995177+Vutranyennhi@users.noreply.github.com>
2023-06-15 12:53:49 +0700

    Add files via upload

---
+++ main3.java
@@ -0,0 +1,17 @@
+
+public class main3 {
+		public static void main3( String[] args) {
+			TamGiac tg1 = new TamGiac(6,45,75);
+			TamGiac tg2 = new TamGiac(3,3,9);
+			TamGiac tg3 = new TamGiac(3,3,3);
+			TamGiac tg4 = new TamGiac(1,2,3);
+			TamGiac tg5 = new TamGiac(4,5,6);
+			System.out.println(tg1);
+			System.out.println(tg2);
+			System.out.println(tg3);
+			System.out.println(tg4);
+			System.out.println(tg5);
+		}
+	}
+
+}
