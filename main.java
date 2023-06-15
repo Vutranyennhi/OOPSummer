492f4c058e54a632bcae23841c706ae4262a0868
Vutranyennhi <135995177+Vutranyennhi@users.noreply.github.com>
2023-06-15 12:57:22 +0700

    Add files via upload

--- main.java
+++ main.java
@@ -1,15 +1,12 @@
+package bai1_ToaDo;
 
 public class main {
-	public static void main( String[] args) {
-		TamGiac tg1 = new TamGiac(6,45,75);
-		TamGiac tg2 = new TamGiac(3,3,9);
-		TamGiac tg3 = new TamGiac(3,3,3);
-		TamGiac tg4 = new TamGiac(1,2,3);
-		TamGiac tg5 = new TamGiac(4,5,6);
-		System.out.println(tg1);
-		System.out.println(tg2);
-		System.out.println(tg3);
-		System.out.println(tg4);
-		System.out.println(tg5);
+
+	public static void main(String[] args) {
+		Toado toado 
+		= new Toado();
+		System.out.println(toado);
 	}
+
 }
+
